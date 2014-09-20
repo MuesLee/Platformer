d5c97c4f064144b3d5980db5cd13dd3c8e6784c2
MuesLee <stimo@gmx.net>
2014-09-20 14:45:38 +0200

    Added Stuff to GameEntity

---
+++ platformer.coop/src/main/java/platformer/coop/entities/Animation.java
@@ -0,0 +1,5 @@
+package platformer.coop.entities;
+
+public class Animation {
+
+}
--- platformer.coop/src/main/java/platformer/coop/entities/GameEntity.java
+++ platformer.coop/src/main/java/platformer/coop/entities/GameEntity.java
@@ -1,96 +1,135 @@
 package platformer.coop.entities;
 
+import java.awt.Rectangle;
+
+import platformer.coop.tilemap.TileMap;
+
 public abstract class GameEntity
 {
+	
+	protected TileMap tileMap;
+	protected int tileSize;
+	protected int xMap;
+	protected int yMap;
+	
+	protected int x;
+	protected int y;
+	protected int dx;
+	protected int dy;
+	
+	protected int width;
+	protected int height;
+	
+	protected int collisionBoxWidth;
+	protected int collisionBoxHeight;
+	
+	protected int currentRow;
+	protected int currentColumn;
+	protected int xDestination;
+	protected int yDestination;
+	protected int xTemp;
+	protected int yTemp;
+	
+	protected boolean bottomLeft;
+	protected boolean bottomRight;
+	protected boolean topLeft;
+	protected boolean topRight;
+	
+	protected Animation animation;
+	protected int currentAction;
+	protected int previousAction;
+	protected boolean isFacingRight;
+	
+	protected boolean isMovingLeft = false;
+	protected boolean isMovingRight = false;
+	protected boolean isJumping = false;
+	protected boolean isCrouching = false;
+	protected boolean isShooting = false;
+	protected boolean isFalling = false;
 
-	public boolean isMovingLeft()
+	protected double moveSpeed;
+	protected double moveSpeedMax;
+	protected double moveSpeedSlowDownRate;
+	protected double fallSpeed;
+	protected double jumpStart;
+	protected double jumpMax;
+
+	protected String name;
+
+	
+	
+	
+	public GameEntity(TileMap tileMap) {
+		super();
+		this.tileMap = tileMap;
+		this.tileSize = tileMap.getTileSize();
+	}
+	
+	public boolean intersects(GameEntity entity){
+		Rectangle recThis = getCollisionBox();
+		Rectangle recOther = entity.getCollisionBox();
+		
+		return recThis.intersects(recOther);
+	}
+	
+	public Rectangle getCollisionBox()
 	{
+		return new Rectangle(x-collisionBoxWidth, y-collisionBoxHeight, collisionBoxWidth, collisionBoxHeight);
+	}
+
+	public boolean isMovingLeft() {
 		return isMovingLeft;
 	}
 
-	public void setMovingLeft(boolean isMovingLeft)
-	{
+	public void setMovingLeft(boolean isMovingLeft) {
 		this.isMovingLeft = isMovingLeft;
 	}
 
-	public boolean isMovingRight()
-	{
+	public boolean isMovingRight() {
 		return isMovingRight;
 	}
 
-	public void setMovingRight(boolean isMovingRight)
-	{
+	public void setMovingRight(boolean isMovingRight) {
 		this.isMovingRight = isMovingRight;
 	}
 
-	public boolean isJumping()
-	{
+	public boolean isJumping() {
 		return isJumping;
 	}
 
-	public void setJumping(boolean isJumping)
-	{
+	public void setJumping(boolean isJumping) {
 		this.isJumping = isJumping;
 	}
 
-	public boolean isCrouching()
-	{
+	public boolean isCrouching() {
 		return isCrouching;
 	}
 
-	public void setCrouching(boolean isCrouching)
-	{
+	public void setCrouching(boolean isCrouching) {
 		this.isCrouching = isCrouching;
 	}
 
-	public boolean isShooting()
-	{
+	public boolean isShooting() {
 		return isShooting;
 	}
 
-	public void setShooting(boolean isShooting)
-	{
+	public void setShooting(boolean isShooting) {
 		this.isShooting = isShooting;
 	}
 
-	public int getMoveSpeed()
-	{
-		return moveSpeed;
-	}
-
-	public void setMoveSpeed(int moveSpeed)
-	{
-		this.moveSpeed = moveSpeed;
-	}
-
-	public int getJumpSpeed()
-	{
-		return jumpSpeed;
-	}
-
-	public void setJumpSpeed(int jumpSpeed)
-	{
-		this.jumpSpeed = jumpSpeed;
-	}
-
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 
-	public void setName(String name)
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	private boolean isMovingLeft = false;
-	private boolean isMovingRight = false;
-	private boolean isJumping = false;
-	private boolean isCrouching = false;
-	private boolean isShooting = false;
+	public boolean isFalling() {
+		return isFalling;
+	}
 
-	private int moveSpeed;
-	private int jumpSpeed;
-
-	private String name;
+	public void setFalling(boolean isFalling) {
+		this.isFalling = isFalling;
+	}
 }
