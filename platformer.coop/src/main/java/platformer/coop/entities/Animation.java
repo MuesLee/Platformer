e59e341f0723806cee26c2df682ee3a661d91210
MuesLee <stimo@gmx.net>
2014-09-21 19:21:36 +0200

    jk

--- platformer.coop/src/main/java/platformer/coop/entities/Animation.java
+++ platformer.coop/src/main/java/platformer/coop/entities/Animation.java
@@ -1,5 +1,55 @@
 package platformer.coop.entities;
 
+import java.awt.image.BufferedImage;
+
 public class Animation {
 
+	private BufferedImage[] frames;
+	private int currentFrame;
+	private long startTime;
+	private long delay;
+
+	private boolean playedOnce = false;
+
+	public Animation() {
+	}
+
+	public void update() {
+
+	}
+
+	public BufferedImage[] getFrames() {
+		return frames;
+	}
+
+	public void setFrames(BufferedImage[] frames) {
+		this.frames = frames;
+		this.currentFrame = 0;
+		startTime = System.nanoTime();
+		playedOnce = false;
+	}
+
+	public int getCurrentFrame() {
+		return currentFrame;
+	}
+
+	public void setCurrentFrame(int currentFrame) {
+		this.currentFrame = currentFrame;
+	}
+
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(long startTime) {
+		this.startTime = startTime;
+	}
+
+	public long getDelay() {
+		return delay;
+	}
+
+	public void setDelay(long delay) {
+		this.delay = delay;
+	}
 }
